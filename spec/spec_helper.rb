da52da932dcff9203ecdb4758d0966e01da37955
matviibodnar <matvii.bodnar@walltech.me>
2025-02-19 14:06:00 +0200

    fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,9 +8,14 @@ require 'allure-rspec'
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.run_server = false
-  config.add_argument('--no-sandbox')
-  config.add_argument('--disable-dev-shm-usage')
-  config.add_argument('--disable-extensions')
+
+  options = Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-extensions')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 AllureRspec.configure do |config|
