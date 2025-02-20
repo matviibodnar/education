15eeb56b31f08e3c92c663226bfce2c15d9e7d56
matviibodnar <matvii.bodnar@walltech.me>
2025-02-20 17:33:48 +0200

    added small test for modal

---
+++ spec/features/herokuapp/modal_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../../../utils/herokuapp/alertutils'
+require_relative '../../../utils/texts'
+
+RSpec.feature 'Modals', type: :feature do
+  scenario 'Check and accept' do
+    visit 'https://the-internet.herokuapp.com/entry_ad'
+
+    expect(find_by_id('modal').text).to eq(Texts::MODAL_TEXT)
+
+    click_button 'Close'
+  end
+end
